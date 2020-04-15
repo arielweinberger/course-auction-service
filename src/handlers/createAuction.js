56ce0d6f74cddf2ed993d9862a75334c27e53baf
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-15 21:52:44 +0200

    Creating an auction (Part 2)

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,17 @@
 async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString(),
+  };
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    statusCode: 201,
+    body: JSON.stringify(auction),
   };
 }
 
 export const handler = createAuction;
-
-
