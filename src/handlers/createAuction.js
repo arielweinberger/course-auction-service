07fbd5cf72464839719e2089654c87bb8cb1d1db
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-18 17:44:52 +0200

    Auction insert to DynamoDB

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,12 +1,23 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function createAuction(event, context) {
   const { title } = JSON.parse(event.body);
   const now = new Date();
 
   const auction = {
+    id: uuid(),
     title,
     status: 'OPEN',
     createdAt: now.toISOString(),
   };
+
+  await dynamodb.put({
+    TableName: 'AuctionsTable',
+    Item: auction,
+  }).promise();
 
   return {
     statusCode: 201,
