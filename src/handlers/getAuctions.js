a6579116a82234d8b6ec67d50748ec1458223217
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-20 23:41:21 +0200

    CRUD: Get All Auctions

---
+++ src/handlers/getAuctions.js
@@ -0,0 +1,33 @@
+import AWS from 'aws-sdk';
+import middy from '@middy/core';
+import httpJsonBodyParser from '@middy/http-json-body-parser';
+import httpEventNormalizer from '@middy/http-event-normalizer';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function getAuctions(event, context) {
+  let auctions;
+
+  try {
+    const result = await dynamodb.scan({
+      TableName: process.env.AUCTIONS_TABLE_NAME
+    }).promise();
+
+    auctions = result.Items;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(auctions),
+  };
+}
+
+export const handler = middy(getAuctions)
+  .use(httpJsonBodyParser())
+  .use(httpEventNormalizer())
+  .use(httpErrorHandler());
