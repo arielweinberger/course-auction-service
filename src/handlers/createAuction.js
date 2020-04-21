4fc30d893a10487c57aa33e93760f1bf0576d83a
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-21 23:16:00 +0200

    CRUD: Placing a bid

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -14,6 +14,9 @@ async function createAuction(event, context) {
     title,
     status: 'OPEN',
     createdAt: now.toISOString(),
+    highestBid: {
+      amount: 0,
+    },
   };
 
   try {
---
+++ src/handlers/placeBid.js
@@ -0,0 +1,37 @@
+import AWS from 'aws-sdk';
+import commonMiddleware from '../lib/commonMiddleware';
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function placeBid(event, context) {
+  const { id } = event.pathParameters;
+  const { amount } = event.body;
+
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id },
+    UpdateExpression: 'set highestBid.amount = :amount',
+    ExpressionAttributeValues: {
+      ':amount': amount,
+    },
+    ReturnValues: 'ALL_NEW',
+  };
+
+  let updatedAuction;
+
+  try {
+    const result = await dynamodb.update(params).promise();
+    updatedAuction = result.Attributes;
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(updatedAuction),
+  };
+}
+
+export const handler = commonMiddleware(placeBid);
