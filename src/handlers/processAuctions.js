17ec7026cde940457019de8464d398bb2ad525be
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-22 22:22:09 +0200

    Closing auctions

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,8 +1,17 @@
+import createError from 'http-errors';
 import { getEndedAuctions } from '../lib/getEndedAuctions';
+import { closeAuction } from '../lib/closeAuction';
 
 async function processAuctions(event, context) {
-  const auctionsToClose = await getEndedAuctions();
-  console.log(auctionsToClose);
+  try {
+    const auctionsToClose = await getEndedAuctions();
+    const closePromises = auctionsToClose.map(auction => closeAuction(auction));
+    await Promise.all(closePromises);
+    return { closed: closePromises.length };
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 }
 
 export const handler = processAuctions;
---
+++ src/lib/closeAuction.js
@@ -0,0 +1,20 @@
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export async function closeAuction(auction) {
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    Key: { id: auction.id },
+    UpdateExpression: 'set #status = :status',
+    ExpressionAttributeValues: {
+      ':status': 'CLOSED',
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  };
+
+  const result = await dynamodb.update(params).promise();
+  return result;
+}
