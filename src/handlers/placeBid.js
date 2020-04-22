1d01227e800a899237aad0537aed34ff5e857e14
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-22 22:22:15 +0200

    Validation: Bidding on Closed Auctions

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -11,6 +11,10 @@ async function placeBid(event, context) {
 
   const auction = await getAuctionById(id);
 
+  if (auction.status !== 'OPEN') {
+    throw new createError.Forbidden(`You cannot bid on closed auctions!`);
+  }
+
   if (amount <= auction.highestBid.amount) {
     throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
   }
