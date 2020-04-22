6e4d9cde6da9819bb748411477fdd8106820c47c
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-22 21:48:56 +0200

    Identify auctions that need to be closed

--- src/handlers/processAuctions.js
+++ src/handlers/processAuctions.js
@@ -1,5 +1,8 @@
+import { getEndedAuctions } from '../lib/getEndedAuctions';
+
 async function processAuctions(event, context) {
-  console.log('processing auctions!');
+  const auctionsToClose = await getEndedAuctions();
+  console.log(auctionsToClose);
 }
 
 export const handler = processAuctions;
---
+++ src/lib/getEndedAuctions.js
@@ -0,0 +1,22 @@
+import AWS from 'aws-sdk';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export async function getEndedAuctions() {
+  const now = new Date();
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status AND endingAt <= :now',
+    ExpressionAttributeValues: {
+      ':status': 'OPEN',
+      ':now': now.toISOString(),
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  };
+
+  const result = await dynamodb.query(params).promise();
+  return result.Items;
+}
