fd7a1d77daff6b6c46e08084945092e7b59eaf40
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-23 23:19:13 +0200

    Schema validation for createAuction

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,9 @@
 import { v4 as uuid } from 'uuid';
 import AWS from 'aws-sdk';
-import commonMiddleware from '../lib/commonMiddleware';
 import createError from 'http-errors';
+import validator from '@middy/validator';
+import commonMiddleware from '../lib/commonMiddleware';
+import createAuctionSchema from '../lib/schemas/createAuctionSchema';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -38,4 +40,5 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = commonMiddleware(createAuction);
+export const handler = commonMiddleware(createAuction)
+  .use(validator({ inputSchema: createAuctionSchema }));
---
+++ src/lib/schemas/createAuctionSchema.js
@@ -0,0 +1,16 @@
+const schema = {
+  properties: {
+    body: {
+      type: 'object',
+      properties: {
+        title: {
+          type: 'string',
+        },
+      },
+      required: ['title'],
+    },
+  },
+  required: ['body'],
+};
+
+export default schema;
