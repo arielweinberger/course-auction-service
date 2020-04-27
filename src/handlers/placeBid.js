4da395bc09cbec72540ce7117dbc3cc60ff1d661
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-27 20:23:27 +0200

    Specifying identity of bidder + validation

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -10,13 +10,26 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 async function placeBid(event, context) {
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
 
+  // Bid identity validation
+  if (email === auction.seller) {
+    throw new createError.Forbidden(`You cannot bid on your own auctions!`);
+  }
+
+  // Avoid double bidding
+  if (email === auction.highestBid.bidder) {
+    throw new createError.Forbidden(`You are already the highest bidder`);
+  }
+
+  // Auction status validation
   if (auction.status !== 'OPEN') {
     throw new createError.Forbidden(`You cannot bid on closed auctions!`);
   }
 
+  // Bid amount validation
   if (amount <= auction.highestBid.amount) {
     throw new createError.Forbidden(`Your bid must be higher than ${auction.highestBid.amount}!`);
   }
@@ -24,9 +37,10 @@ async function placeBid(event, context) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression: 'set highestBid.amount = :amount, highestBid.bidder = :bidder',
     ExpressionAttributeValues: {
       ':amount': amount,
+      ':bidder': email,
     },
     ReturnValues: 'ALL_NEW',
   };
