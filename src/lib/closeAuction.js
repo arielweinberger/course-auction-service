392a607e895e377f5358549f63e6c6c2c74f32f3
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-27 22:34:37 +0200

    Notify seller and bidder when closing an auction

--- src/lib/closeAuction.js
+++ src/lib/closeAuction.js
@@ -1,6 +1,7 @@
 import AWS from 'aws-sdk';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
+const sqs = new AWS.SQS();
 
 export async function closeAuction(auction) {
   const params = {
@@ -15,6 +16,28 @@ export async function closeAuction(auction) {
     },
   };
 
-  const result = await dynamodb.update(params).promise();
-  return result;
+  await dynamodb.update(params).promise();
+
+  const { title, seller, highestBid } = auction;
+  const { amount, bidder } = highestBid;
+
+  const notifySeller = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: 'Your item has been sold!',
+      recipient: seller,
+      body: `Woohoo! Your itme "${title}" has been sold for $${amount}.`,
+    }),
+  }).promise();
+
+  const notifyBidder = sqs.sendMessage({
+    QueueUrl: process.env.MAIL_QUEUE_URL,
+    MessageBody: JSON.stringify({
+      subject: 'You won an auction!',
+      recipient: bidder,
+      body: `What a great deal! You got yourself a "${title}" for $${amount}.`,
+    }),
+  }).promise();
+
+  return Promise.all([notifySeller, notifyBidder]);
 }
