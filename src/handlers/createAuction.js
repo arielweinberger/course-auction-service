e2b485d88836ad2bac1a0298f63466f381713dfd
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-04-27 20:16:01 +0200

    Set seller identity

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -9,6 +9,7 @@ const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 async function createAuction(event, context) {
   const { title } = event.body;
+  const { email } = event.requestContext.authorizer;
   const now = new Date();
   const endDate = new Date();
   endDate.setHours(now.getHours() + 1);
@@ -22,6 +23,7 @@ async function createAuction(event, context) {
     highestBid: {
       amount: 0,
     },
+    seller: email,
   };
 
   try {
