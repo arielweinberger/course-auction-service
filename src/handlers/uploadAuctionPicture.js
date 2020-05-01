558de2924ec15076f53b0d74087ee441b6a49d06
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-05-01 23:45:47 +0200

    Validate auction ownership

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -7,7 +7,14 @@ import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
 
 export async function uploadAuctionPicture(event) {
   const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
   const auction = await getAuctionById(id);
+
+  // Validate auction ownership
+  if (auction.seller !== email) {
+    throw new createError.Forbidden(`You are not the seller of this auction!`);
+  }
+
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64, 'base64');
 
