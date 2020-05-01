8d9f6ba38dc103b4977d876b601a5d0a80807687
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-05-01 23:03:40 +0200

    middy-error-handling

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,3 +1,6 @@
+import middy from '@middy/core';
+import httpErrorHandler from '@middy/http-error-handler';
+import createError from 'http-errors';
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
 
@@ -7,8 +10,13 @@ export async function uploadAuctionPicture(event) {
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64, 'base64');
 
-  const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
-  console.log(uploadToS3Result);
+  try {
+    const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
+    console.log(uploadToS3Result);
+  } catch (error) {
+    console.error(error);
+    throw new createError.InternalServerError(error);
+  }
 
   return {
     statusCode: 200,
@@ -16,4 +24,5 @@ export async function uploadAuctionPicture(event) {
   };
 }
 
-export const handler = uploadAuctionPicture;
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler());
