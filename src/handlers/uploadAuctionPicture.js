9e20093e4c99cb0f761602e29b7bf9bb13e0bf99
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-05-01 22:38:30 +0200

    Upload picture to S3

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,4 +1,15 @@
+import { getAuctionById } from './getAuction';
+import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
+
 export async function uploadAuctionPicture(event) {
+  const { id } = event.pathParameters;
+  const auction = await getAuctionById(id);
+  const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
+  const buffer = Buffer.from(base64, 'base64');
+
+  const uploadToS3Result = await uploadPictureToS3(auction.id + '.jpg', buffer);
+  console.log(uploadToS3Result);
+
   return {
     statusCode: 200,
     body: JSON.stringify({}),
---
+++ src/lib/uploadPictureToS3.js
@@ -0,0 +1,15 @@
+import AWS from 'aws-sdk';
+
+const s3 = new AWS.S3();
+
+export async function uploadPictureToS3(key, body) {
+  const result = await s3.upload({
+    Bucket: process.env.AUCTIONS_BUCKET_NAME,
+    Key: key,
+    Body: body,
+    ContentEncoding: 'base64',
+    ContentType: 'image/jpeg',
+  }).promise();
+
+  return result;
+}
