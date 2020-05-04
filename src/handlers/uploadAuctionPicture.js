ad9a60f008eb4209ec05c685f024c88350a2dcf4
Ariel Weinberger <weinberger.ariel@gmail.com>
2020-05-04 15:12:33 +0200

    Set up CORS

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,6 +1,7 @@
 import middy from '@middy/core';
 import httpErrorHandler from '@middy/http-error-handler';
 import validator from '@middy/validator';
+import cors from '@middy/http-cors';
 import createError from 'http-errors';
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
@@ -38,4 +39,5 @@ export async function uploadAuctionPicture(event) {
 
 export const handler = middy(uploadAuctionPicture)
   .use(httpErrorHandler())
-  .use(validator({ inputSchema: uploadAuctionPictureSchmea }));
+  .use(validator({ inputSchema: uploadAuctionPictureSchmea }))
+  .use(cors());
